d03719b44cf4cf4a52478c6876863c5b1927815a
Tom Bertalan <tom@tombertalan.com>
2017-11-06 11:42:13 -0500

    Send big arrays over socket with zlib compression and halving.

--- src/pipe.py
+++ src/pipe.py
@@ -20,10 +20,29 @@ numpy.ndarray
 999999
 
 '''
-import socket, time, pickle, struct
-
 # https://stackoverflow.com/questions/34653875/python-how-to-send-data-over-tcp
 # https://wiki.python.org/moin/TcpCommunication
+
+import socket, time, pickle, struct
+from contextlib import contextmanager
+
+@contextmanager
+def timeit(label=None):
+    if label is not None: print('%s ... ' % label, end='')
+    s = time.time()
+    yield
+    e = time.time()
+    print('(%.1f sec).' % (e - s))
+
+def _compress(data):
+    import zlib
+    with timeit('Compressing %d bytes' % len(data)):
+        return zlib.compress(data)
+
+def _decompress(data):
+    import zlib
+    with timeit('Decompressing %d bytes' % len(data)):
+        return zlib.decompress(data)
 
 
 class Sink(object):
@@ -38,14 +57,31 @@ class Sink(object):
     def __del__(self):
         self.s.close()
 
-    def put(self, msg):
+    def put(self, msg, compress=False):
         msg = pickle.dumps(msg)
+        if compress:
+            msg = _compress(msg)
         # Prefix each message with a 4-byte length (network byte order)
         self.s.sendall(
             struct.pack('>I', len(msg))
             +
             msg
         )
+
+    def putArray(self, arr, recursiveHalving=0, **kwargs):
+        kwargs.setdefault('compress', True)
+        def halves(x):
+            l = len(x)
+            s = int(float(l) / 2)
+            return x[:s], x[s:]
+        assert recursiveHalving >= 0
+        try:    
+            if recursiveHalving == 0:
+                self.put(arr, **kwargs)
+            else:
+                [self.putArray(x, recursiveHalving-1) for x in halves(arr)]
+        except MemoryError:
+            self.putArray(arr, recursiveHalving+1)
 
 
 class Source(object):
@@ -63,7 +99,7 @@ class Source(object):
             self.conn, self.connAddr = self.s.accept()
             print('Connected by', self.connAddr)
 
-    def get(self, sleep=1, connect=True, pbar=False):
+    def get(self, sleep=1, connect=True, pbar=False, decompress=False):
         if connect:
             self._connect()
         while True:
@@ -79,6 +115,8 @@ class Source(object):
                 out = self._recvall(msglen, pbar=pbar)
                 break
         if out is not None:
+            if decompress:
+                out = _decompress(out)
             out = pickle.loads(out)
         return out
 
@@ -94,11 +132,42 @@ class Source(object):
             if not packet:
                 return None
             data += packet
-        if pbar: pbar.update(n - pbar.n)
+        if pbar: 
+            pbar.update(n - pbar.n)
+            pbar.refresh()
         return data
 
     def __del__(self):
         if hasattr(self, 'conn'):
             self.conn.close()
         self.s.close()
+
+
+class ArraySaver(Source):
+
+    def __init__(self, pathFormat='./message_%s', **kwargs):
+        Source.__init__(self, **kwargs)
+        self.pathFormat = pathFormat
+
+    def record(self, compressed=False, **getKwargs):
+        getKwargs.setdefault('decompress', True)
+        import numpy as np
+        while True:
+            try:
+                msg = self.get(**getKwargs)
+                print('Got msg ...', end=' ')
+                if isinstance(msg, np.ndarray):
+                    print('array shape is %s.' % (msg.shape,))
+                    fpath = self.pathFormat % time.time()
+                    with timeit('Saving to %s' % fpath):
+                        if compressed:
+                            np.savez_compressed(fpath, msg)
+                        else:
+                            np.save(fpath, msg)
+                else:
+                    from warnings import warn
+                    warn('got %s, not ndarray:\n%s' % (type(msg), msg))
+            except KeyboardInterrupt:
+                print('Caught KeyboardInterrupt; stopping %s.' % type(self).__name__)
+                break
 
