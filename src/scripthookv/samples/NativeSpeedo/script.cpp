efcdfc73c9aaeb78e1aa796b4111977003ff7e1f
Tom Bertalan <tom@tombertalan.com>
2021-09-06 07:49:17 -0400

    This ScriptHookV seems like it could be useful.

--- src/scripthookv/samples/NativeSpeedo/script.cpp
+++ src/scripthookv/samples/NativeSpeedo/script.cpp
@@ -6,6 +6,7 @@
 
 #include "script.h"
 #include "utils.h"
+#include <vector>
 
 int idTextureSpeedoBack, idTextureSpeedoArrow;
 
@@ -63,7 +64,25 @@ void update()
 
 	// draw textures
 	draw_speedo(speed, speedoAlpha);
-}
+
+	// Get nearby pedestrians.
+	//int *sizeAndPeds;
+	//PED::GET_PED_NEARBY_PEDS(playerPed, sizeAndPeds, )
+
+	std::vector<Ped> peds;
+	const int ARR_SIZE = 1024; //max size of array to hold all the peds
+	Ped worldPeds[ARR_SIZE]; //array to hold all the peds
+	int numPedsInWorld = worldGetAllPeds(worldPeds, ARR_SIZE); //fills up worldPeds with peds, and returns the number of peds found as an int in numPedsInWorld
+	//for (int i = 0; i < numPedsInWorld; i++){     
+	//	peds.push_back(worldPeds[i]); //move the peds you want from the array to the vector, because the vector is far more versatile}
+	//									  //You can also use this in exactly the same way with objects and vehicles, using:int worldGetAllVehicles(int *arr, int arrSize);
+	//									  //and int worldGetAllObjects(int *arr, int arrSize);
+	//									  //these three functions are newly added to the ScriptHookV SDK by Alexander Blade, and their range from the player 
+	//	// is only limited by the max range that the player can affect such entities naturally, unlike the very-similar-but-much-shorter-range PED::GET_PED_NEARBY_PEDS() and GET_PED_NEARBY_VEHICLES() natives. 
+	//	// Open "pools.sln" (or its script.cpp file) from the "samples" folder in the scriptHookV SDK download if you want to see more.
+	//}
+
+	GAMEPLAY::
 
 void main()
 {	
